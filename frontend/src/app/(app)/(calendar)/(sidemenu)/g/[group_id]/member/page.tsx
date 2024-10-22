85c1409829e1f06a6233cb67cc870cb3e2e0f64c
chell-uoxou <chelluoxou@gmail.com>
2024-10-22 18:21:47 +0900

    feat とりあえずdata-tableを置いてみる

--- frontend/src/app/(app)/(calendar)/(sidemenu)/g/[group_id]/member/page.tsx
+++ frontend/src/app/(app)/(calendar)/(sidemenu)/g/[group_id]/member/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+"use client";
+import SmallTitleWithIcon from "~/components/common/SmallTitleWithIcon";
+import { DataTableDemo } from "~/features/memberList/GroupMemberDataTable";
 
 function Page() {
-  return <div>ここはグループのメンバー</div>;
+  return (
+    <div className="p-6 flex flex-col gap-3">
+      <SmallTitleWithIcon title="メンバー一覧" />
+      <DataTableDemo />
+    </div>
+  );
 }
 
 export default Page;
