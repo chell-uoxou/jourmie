9f743399e17513651d9a22f6d0b9ae4aa70c8624
chell-uoxou <chelluoxou@gmail.com>
2024-12-17 21:03:40 +0900

    feat 手前のドロワーと合うように細かくレイアウトを修正

--- frontend/src/features/eventPool/EventInputDialog.tsx
+++ frontend/src/features/eventPool/EventInputDialog.tsx
@@ -1,12 +1,13 @@
+import { Button } from "~/components/ui/button";
 import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  DialogDescription,
-} from "~/components/ui/dialog";
-import { DialogHeader } from "~/components/ui/dialog";
-import EventForm from "./components/EventForm";
-import { ScrollArea } from "~/components/ui/scroll-area";
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+} from "~/components/ui/drawer";
 
 interface EventInputDialogProps {
   isOpen: boolean;
@@ -15,18 +16,37 @@ interface EventInputDialogProps {
 
 export const EventInputDialog = (props: EventInputDialogProps) => {
   return (
-    <Dialog open={props.isOpen} onOpenChange={props.onOpenChange}>
-      <DialogContent className="w-[440px]">
-        <DialogHeader>
-          <DialogTitle>イベント候補 新規作成</DialogTitle>
-          <DialogDescription>
-            行ってみたいイベントを追加してみましょう‼︎
-          </DialogDescription>
-        </DialogHeader>
-        <ScrollArea className="h-96 ">
-          <EventForm />
-        </ScrollArea>
-      </DialogContent>
-    </Dialog>
+    // <Dialog open={props.isOpen} onOpenChange={props.onOpenChange}>
+    //   <DialogContent className="w-[440px]">
+    //     <DialogHeader>
+    //       <DialogTitle>イベント候補 新規作成</DialogTitle>
+    //       <DialogDescription>
+    //         行ってみたいイベントを追加してみましょう‼︎
+    //       </DialogDescription>
+    //     </DialogHeader>
+    //     <ScrollArea className="h-96 ">
+    //       <EventForm />
+    //     </ScrollArea>
+    //   </DialogContent>
+    // </Dialog>
+    <Drawer
+      open={props.isOpen}
+      onOpenChange={props.onOpenChange}
+      modal={false}
+      direction="left"
+    >
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 };
