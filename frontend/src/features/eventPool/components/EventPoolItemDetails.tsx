6a980cb44cab63a09b1e5e79592be283ff22e694
sakata <is0670pk@ed.ritsumei.ac.jp>
2025-02-09 21:03:10 +0900

    元となるデザイン完成

--- frontend/src/features/eventPool/components/EventPoolItemDetails.tsx
+++ frontend/src/features/eventPool/components/EventPoolItemDetails.tsx
@@ -1,8 +1,21 @@
-import { CalendarPlus, Pencil, Trash2, X } from "lucide-react";
+import {
+  CalendarPlus,
+  Pencil,
+  Trash2,
+  X,
+  Map,
+  CalendarRange,
+  Hourglass,
+  Check,
+  Text,
+} from "lucide-react";
 import SmallIconButton from "~/components/common/SmallIconButton";
 import SmallTitleWithIcon from "~/components/common/SmallTitleWithIcon";
 import { PopoverContent } from "~/components/ui/popover";
 import { DBEventPoolItem } from "~/lib/firestore/utils";
+import { Timestamp } from "firebase/firestore";
+import { TimeRange } from "~/models/types/common";
+import formatDuration from "~/utils/formater";
 
 interface EventPoolItemDetailsProps {
   eventPoolItem: DBEventPoolItem;
@@ -13,25 +26,79 @@ interface EventPoolItemDetailsProps {
 }
 
 const EventPoolItemDetails = (props: EventPoolItemDetailsProps) => {
+  const formatTimes = (times: TimeRange[]) => {
+    const { start_time, end_time } = times[0] ?? {
+      start_time: Timestamp.now(),
+      end_time: Timestamp.now(),
+    };
+
+    const startDate = start_time.toDate();
+    const endDate = end_time.toDate();
+
+    const sameDay = startDate.toDateString() === endDate.toDateString();
+
+    const startFormatted = startDate.toLocaleString("ja-JP", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    const endFormatted = endDate.toLocaleString("ja-JP", {
+      hour: "2-digit",
+      minute: "2-digit",
+      ...(sameDay ? {} : { year: "numeric", month: "numeric", day: "numeric" }),
+    });
+
+    return sameDay
+      ? `${startFormatted} ~ ${endFormatted}`
+      : `${startFormatted} ~ ${endFormatted}`;
+  };
+
   return (
     <PopoverContent className="w-80" side={"right"} sideOffset={10}>
-      <div className="flex flex-col gap-2">
-        <div className="flex gap-2 justify-end w-full ">
-          <div className="flex gap-0">
-            <SmallIconButton icon={<Pencil />} onClick={props.onClickEdit} />
-            <SmallIconButton
-              icon={<CalendarPlus />}
-              onClick={props.onClickAddToCalendar}
-            />
-            <SmallIconButton icon={<Trash2 />} onClick={props.onClickDelete} />
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-end w-full ">
+            <div className="flex gap-0">
+              <SmallIconButton icon={<Pencil />} onClick={props.onClickEdit} />
+              <SmallIconButton
+                icon={<CalendarPlus />}
+                onClick={props.onClickAddToCalendar}
+              />
+              <SmallIconButton
+                icon={<Trash2 />}
+                onClick={props.onClickDelete}
+              />
+            </div>
+            <SmallIconButton icon={<X />} onClick={props.onClickClose} />
           </div>
-          <SmallIconButton icon={<X />} onClick={props.onClickClose} />
+          <SmallTitleWithIcon title={props.eventPoolItem.title} />
+          <div>{props.eventPoolItem.description}</div>
         </div>
-        <SmallTitleWithIcon title={props.eventPoolItem.title} />
-        <div>{props.eventPoolItem.description}</div>
-        <div>{props.eventPoolItem.location_text}</div>
-        <div>{props.eventPoolItem.preparation_task}</div>
-        <div>{props.eventPoolItem.notes}</div>
+        <div className="flex flex-col gap-3">
+          <div className="flex gap-2 items-center text-sm">
+            <Map className="size-4" />
+            {props.eventPoolItem.location_text}
+          </div>
+          <div className="flex gap-2 items-center text-sm">
+            <CalendarRange className="size-4" />
+            {formatTimes(props.eventPoolItem.available_times)}
+          </div>
+          <div className="flex gap-2 items-center text-sm">
+            <Hourglass className="size-4" />
+            {formatDuration(props.eventPoolItem.default_duration)}
+          </div>
+          <div className="flex gap-2 items-center text-sm">
+            <Check className="size-4" />
+            {props.eventPoolItem.preparation_task}
+          </div>
+          <div className="flex gap-2 items-center text-sm">
+            <Text className="size-4" />
+            {props.eventPoolItem.notes}
+          </div>
+        </div>
       </div>
     </PopoverContent>
   );
