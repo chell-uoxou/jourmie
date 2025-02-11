9a2933e162a33b9afb2e622c0bc0c22e3bf86d84
shivasaki <shivasaki0329@gmail.com>
2025-02-12 06:04:54 +0900

    アカウントボタンにメニュー、ログアウトを追加

--- frontend/src/features/appLayout/AppTopBar.tsx
+++ frontend/src/features/appLayout/AppTopBar.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { getAuth, signOut } from "firebase/auth";
 import { GroupSwitcher } from "./components/GroupSwitcher";
 import { LogoIcon } from "~/components/common/LogoIcon";
 import useCurrentAccount from "~/hooks/useCurrentAccount";
 import { useEffect, useState } from "react";
 import MyAvatar from "./components/MyAvatar";
-import { Bell } from "lucide-react";
+import { Bell, LogOut, Settings } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import { DBGroup } from "~/lib/firestore/schemas";
 import useGroupRouter from "~/hooks/useGroupRouter";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 
 export const AppTopBar = () => {
   const [groups, setGroups] = useState<DBGroup[] | null | "loading">("loading");
@@ -23,6 +31,18 @@ export const AppTopBar = () => {
     });
   }, [currentDBAccount, getGroupsByAccount]);
 
+  const router = useRouter();
+  const auth = getAuth();
+  const handleLogOut = () => {
+    signOut(auth)
+      .then(() => {
+        router.push("/account/login");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="flex justify-between items-center h-14 px-6 py-2 border-b border-brand-border-color absolute w-screen">
       <GroupSwitcher
@@ -36,7 +56,21 @@ export const AppTopBar = () => {
         <LogoIcon />
       </div>
       <div className="flex flex-row-reverse w-[200px] items-center gap-3">
-        <MyAvatar />
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MyAvatar />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <Settings />
+              設定
+            </DropdownMenuItem>
+            <DropdownMenuItem >
+              <LogOut />
+              ログアウト
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <Button variant="ghost" size="icon" className="size-8">
           <Bell size={20} />
         </Button>
