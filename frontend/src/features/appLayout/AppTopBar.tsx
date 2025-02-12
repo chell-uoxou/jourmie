2b7ec545fc5b75f3b0eae6454651bf9a5a618572
shivasaki <shivasaki0329@gmail.com>
2025-02-12 17:51:56 +0900

    不要箇所を削除

--- frontend/src/features/appLayout/AppTopBar.tsx
+++ frontend/src/features/appLayout/AppTopBar.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import { getAuth, signOut } from "firebase/auth";
 import { GroupSwitcher } from "./components/GroupSwitcher";
 import { LogoIcon } from "~/components/common/LogoIcon";
 import useCurrentAccount from "~/hooks/useCurrentAccount";
 import { useEffect, useState } from "react";
-import { Bell} from "lucide-react";
+import { Bell } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import { DBGroup } from "~/lib/firestore/schemas";
 import useGroupRouter from "~/hooks/useGroupRouter";
-import { useRouter } from "next/navigation";
 import { AccountMenuContent } from "./components/AccountMenuContent";
 
 export const AppTopBar = () => {
@@ -25,18 +23,6 @@ export const AppTopBar = () => {
     });
   }, [currentDBAccount, getGroupsByAccount]);
 
-  const router = useRouter();
-  const auth = getAuth();
-  const handleLogOut = () => {
-    signOut(auth)
-      .then(() => {
-        router.push("/account/login");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
   return (
     <div className="flex justify-between items-center h-14 px-6 py-2 border-b border-brand-border-color absolute w-screen">
       <GroupSwitcher
@@ -50,7 +36,7 @@ export const AppTopBar = () => {
         <LogoIcon />
       </div>
       <div className="flex flex-row-reverse w-[200px] items-center gap-3">
-        <AccountMenuContent/>
+          <AccountMenuContent />
         <Button variant="ghost" size="icon" className="size-8">
           <Bell size={20} />
         </Button>
