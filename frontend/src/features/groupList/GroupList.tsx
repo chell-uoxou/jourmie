4665afb786097c051bef4f2def181dfe3a65d62a
hiromuota166 <otahiromu166@gmail.com>
2025-02-13 20:13:21 +0900

    fix グループ一覧をコンポーネントに細分化

--- frontend/src/features/groupList/GroupList.tsx
+++ frontend/src/features/groupList/GroupList.tsx
@@ -4,13 +4,12 @@ import { Card } from '~/components/ui/card';
 import { LoadingSpinner } from '~/components/ui/spinner';
 import useCurrentAccount from '~/hooks/useCurrentAccount';
 import { DBGroup } from "~/lib/firestore/schemas";
-import Image from "next/image";
+import { GroupListCard } from "./GroupListCard";
+import { GroupNewCreateCard } from './GroupNewCreateCard';
 
 const GroupList = () => {
   const { currentDBAccount, getGroupsByAccount } = useCurrentAccount(true);
   const [groups, setGroups] = useState<DBGroup[] | null | "loading">("loading");
-  const DEFAULT_ICON_URL =
-    "https://firebasestorage.googleapis.com/v0/b/jourmie-181d8.appspot.com/o/group_icons%2FdefaultIcon.png?alt=media&token=ced5dd5a-f87f-4652-9643-76a8579f1249";
 
   useEffect(() => {
     if (currentDBAccount === "loading" || currentDBAccount === null) return;
@@ -19,10 +18,10 @@ const GroupList = () => {
       setGroups(groups);
     });
   }, [currentDBAccount, getGroupsByAccount]);
-  console.log(groups)
 
   return (
     <div className="flex flex-wrap gap-4">
+      <GroupNewCreateCard />
       {groups === "loading" ? (
         <LoadingSpinner />
       ) : groups === null ? (
@@ -33,23 +32,7 @@ const GroupList = () => {
         (
           <>
             {groups.map((group) => (
-              <Card key={group.uid} className="flex-1 min-w-[250px] h-[200px] p-6 shadow">
-                <div className='flex mb-2'>
-                  <Image
-                    src={group.icon_url || DEFAULT_ICON_URL}
-                    alt={group.name || "group icon"}
-                    width={32}
-                    height={32}
-                    className="flex mr-2 rounded-full"
-                  />
-                  <h1 className='text-xl font-bold'>
-                    {group.name}
-                  </h1>
-                </div>
-                <div className=' line-clamp-3'>
-                  {group.description}
-                </div>
-              </Card>
+              <GroupListCard {...group} />
             ))
             }
             {/* flexの限界 */}
